027c9a244ee9faea7bf0b95f21f22b7948a3660f
ezAakash <oenmazoro@gmail.com>
2025-04-01 23:13:29 +0530

    learned about mongoDB

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
-#isme kya karna hai abhi nahi pata
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="ezAakash",
+    author_email="oenmazoro@gmail.com",
+    packages=find_packages()
+)
